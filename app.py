a7a3f86032736765951dcee0c82a5eec8374c4e7
Isha170209 <ishku1022@gmail.com>
2026-03-09 12:15:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if st.session_state.page == "home":
     with col2:
         logo_path = os.path.join("data","logo.png")
         if os.path.exists(logo_path):
-            st.image(logo_path,width=100, height=200)
+            st.image(logo_path,width=100)
 
     st.write("")
     st.write("")
